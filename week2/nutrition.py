9c40c0e9991ee3f3bc4c4bd01e113421bf1ca48a
Wang Yuqi <wangyuqi2003@gmail.com>
2024-05-28 11:17:44 +0800

    Added nutrition.py

---
+++ week2/nutrition.py
@@ -0,0 +1,39 @@
+# create a dictionary that associates fruits with their calories per portion
+# request for fruit name and access the value for that key
+
+def main():
+    item = input("Item: ").lower()
+    calculate_calories(item)
+
+
+def calculate_calories(fruit):
+    fruits = {
+        "apple": "130",
+        "avocado": "50",
+        "banana": "110",
+        "cantaloupe": "50",
+        "grapefruit": "60",
+        "grapes": "90",
+        "honeydew melon": "50",
+        "kiwifruit": "90",
+        "lemon": "15",
+        "lime": "20",
+        "nectarine": "60",
+        "orange": "80",
+        "peach": "60",
+        "pear": "100",
+        "pineapple": "50",
+        "plums": "70",
+        "strawberries": "50",
+        "sweet cherries": "100",
+        "tangerine": "50",
+        "watermelon": "80"
+    }
+    if fruit in fruits:
+        calorie = fruits[fruit]
+        print(f"Calories: {calorie}")
+    else:
+        return
+
+
+main()
